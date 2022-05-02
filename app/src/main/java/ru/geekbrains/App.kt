10b386890f6b24c2a4ad726f5d58676f87e4968f
kange <kangerts9@gmail.com>
2022-05-02 12:18:03 +0300

    task5

--- app/src/main/java/ru/geekbrains/App.kt
+++ app/src/main/java/ru/geekbrains/App.kt
@@ -1,12 +1,39 @@
 package ru.geekbrains
 
 import android.app.Application
+import androidx.room.Room
 import com.github.terrakok.cicerone.Cicerone
+import ru.geekbrains.data.bd.HistoryDao
+import ru.geekbrains.data.bd.HistoryDataBase
 import ru.geekbrains.navigation.CustomRouter
 
 class App: Application() {
 
-    companion object Navigation {
+    override fun onCreate() {
+        super.onCreate()
+        appInstance = this
+    }
+    companion object {
+        private var appInstance: App? = null
+        private var db: HistoryDataBase? = null
+        private const val DB_NAME = "History.db"
+        fun getHistoryDao(): HistoryDao {
+            if (db == null) {
+                synchronized(HistoryDataBase::class.java) {
+                    if (db == null) {
+                        if (appInstance == null) throw
+                        IllegalStateException("Application is null while creating DataBase")
+                        db = Room.databaseBuilder(
+                            appInstance!!.applicationContext,
+                            HistoryDataBase::class.java,
+                            DB_NAME)
+                            .allowMainThreadQueries()
+                            .build()
+                    }
+                }
+            }
+            return db!!.historyDao()
+        }
 
         private val cicerone: Cicerone<CustomRouter> by lazy {
             Cicerone.create(CustomRouter())
--- app/src/main/java/ru/geekbrains/data/TranslateRepository.kt
+++ app/src/main/java/ru/geekbrains/data/TranslateRepository.kt
@@ -1,6 +1,9 @@
 package ru.geekbrains.data
 
+import ru.geekbrains.mvpuser.AppState
+
 
 interface TranslateRepository {
     suspend fun fetchTranslateByWord(word: String): List<DataModel>
+    suspend fun saveToDB(appState: AppState)
 }
